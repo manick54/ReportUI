9ad10971d61664c8b76525d12ff92ac6e5fc29e2
AhalyaM <37434003+AhalyaM@users.noreply.github.com>
2023-02-13 17:13:46 +0530

    Update MainContent.jsx

--- src/components/mainContent/MainContent.jsx
+++ src/components/mainContent/MainContent.jsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import {makeStyles} from '2material-ui/core/styles';
-import Card from '@material-ui/core/card';
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Pagination from '@material-ui/core/Pagination';
+import Pagination from '@material-ui/lab/Pagination';
 import {reportList} from '../json/ReportJson';
 import Divider from '@material-ui/core/Divider';
 
@@ -14,187 +14,228 @@ import List from '@material-ui/core/List';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-import {filterList} from '../json/FilterJson';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
+import Grid from '@material-ui/core/Grid';
 import star from '../../../star.svg';
 import '../../../App.css';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
-import { useEffect } from 'react';
+import FormLabel from '@material-ui/core/FormLabel';
+import {getFilterListAsync} from '../reportSlice';
+import {filterListJson} from '../reportSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minWidth:275,
-        width:'75%',
-        marginTop:'2%',
-        marginLeft:'10%'
+  root: {
+    minWidth: 275,
+    width:'75%',
+    marginTop:'2%',
+    marginLeft:'10%'
+  },
+  root1: {
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.text.secondary,
+    '& svg': {
+      margin: theme.spacing(1.5),
     },
-    root1: {
-        border: `1px solid ${theme.palette.divider}`,
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: theme.palette.background.paper,
-        color: theme.palette.text.secondary,
-        '& svg': {
-            margin: theme.spacing(1.5),
-        },
-        '& hr': {
-            margin: theme.spacing(0,0.5),
-        },
+    '& hr': {
+      margin: theme.spacing(0, 0.5),
     },
-    bullet: {
-        diaplay: 'inline-block',
-        margin:'0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-    content: {
-        display:"table-cell",
-        width:"70%",
-        height:"150px",
-        padding:"10px",
-        verticalAlign: "middle"
-    },
-    nested: {
-        paddingLeft: theme.spacing(4)
-    }
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  content: {
+    
+    display: "table-cell",
+    width: "70%",
+    height: "150px",
+    padding: "10px",
+    verticalAlign: "middle"
 
-
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  }
 }));
 
 export default function MainContent(props) {
-    const classes = useStyles();
-    const [firstIndex, setFirstIndex] = React.useState(0);
-    const [lastIndex, setLastIndex] = React.useState(5);
-    const [openMenu,setOpenMenu] = React.useState([true,false,false,false]);
-    const [flag, setFlag] = React.useState(true);
-    const [checked, setChecked] = React.useState([]);
-    const [radioValue, setRadioValue] = React.useState("");
+  const classes = useStyles();
+  const [firstIndex, setFirstIndex] = React.useState(0);
+  const [lastIndex, setLastIndex] = React.useState(5);
+  const [openMenu, setOpenMenu] = React.useState([true,false,false,false]);
+  const [flag,setFlag] = React.useState(true);
+  const [checked, setChecked] = React.useState([]);
+  const [radioValue, setRadioValue] = React.useState("");
+  
+  const dispatch = useDispatch();
+  const filterList = useSelector(filterListJson);
 
-    const bull = <span className={classes.bullet}>*</span>;
+  const bull = <span className={classes.bullet}>•</span>;
 
-    useEffect(() => {
-        filterList.map(x=> {
-            if(x.type==="checkbox") {
-                let checkedList = [...checked];
-                for(let i=0;i<x.subMenu.length;i++) {
-                    checkedList.push(false);
-                }
-                setChecked(checkedList);
-            }
-        })
-    }, [])
+  
+  useEffect(() => {
+   dispatch(getFilterListAsync());
 
-    const handleChange = (event,value) => {
-        setFirstIndex(lastIndex*(value-1));
-        setLastIndex(lastIndex*(value-1) + 5);
-    }
+  });
 
-    const handleClick = (index) => (event) => {
-        let open = [...openMenu];
-        open[index] = !open[index];
-        setOpenMenu(open);
-        setFlag(!flag);
-    }
 
-    const handleCheckbox = (index) => (event) => {
+  useEffect(() => {
+    filterList.map(x => {
+      if(x.type==="checkbox") {
         let checkedList = [...checked];
-        checkedList[index] = event.target.checked;
+        for(let i=0;i<x.subMenu.length;i++) {
+          checkedList.push(false);
+        }
         setChecked(checkedList);
+      } 
+    }) 
+}, [])
+
+
+  const handleChange = (event,value) => {
+    setFirstIndex(lastIndex*(value-1));
+    setLastIndex(lastIndex*(value-1) + 5);
+  }
+
+  const handleClick = (index) => (event) => {
+    let open = [...openMenu];
+    open[index] = !open[index];
+    setOpenMenu(open);
+    setFlag(!flag);
+   };
+
+   const handleCheckbox = (index) => (event) => {
+    let checkedList = [...checked];
+    checkedList[index] = event.target.checked;
+    setChecked(checkedList);
+   }
+
+   const handleRadioButton = (event) => {
+      setRadioValue(event.target.value);
+   }
+
+      
+  return (
+      <div  style={{display:"grid", gridTemplateColumns:"20% 80%", "marginLeft":props.drawer ?'20%':'0%'}}>
+      <div>
+      <List>
+        {filterList.map((text, index) => (
+          <div>
+          <ListItem button key={text} onClick={handleClick(index)}>
+            <ListItemText primary={text.menu} />
+            {openMenu[index]==true ? <ExpandLess/> : <ExpandMore/>}
+            </ListItem>
+          <Collapse in={openMenu[index]} timeout="auto" unmountOnExit>
+          <List component="div" >
+      
+            {text.type=="list" && text.subMenu.map(x => {
+              return(
+                <ListItem button key={x.name} className={classes.nested}>
+                <ListItemText primary={x.name} />
+                </ListItem>
+              )
+            })}
+
+            {text.type=="checkbox" && text.subMenu.map((x,i) => {
+              return(
+                <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checked[i]}
+                    onChange={handleCheckbox(i)}
+                    color="primary"
+                  />
+                }
+                label={x.name}
+                style={{marginLeft:'15px'}}
+              />
+              
+                )
+            })}
+
+            {text.type == "radiobutton" && text.subMenu.map((x,i) => {
+              return(
+                <FormControl component="fieldset" 
+                style={{marginLeft:'25px'}}>
+                <RadioGroup value={radioValue} onClick={handleRadioButton} onChange={handleRadioButton}>
+                  <FormControlLabel value={x.name} control={<Radio />} label={x.name} />
+                 </RadioGroup>
+                </FormControl>
+              )
+            })}
+
+
+      </List>
+      </Collapse>
+   
+           
+          </div>
+        ))}
+          </List>  
+          </div>
+
+    <div>
+    {reportList.slice(firstIndex,lastIndex).map((text, index) => (
+        <Card className={classes.root}>
+      <CardContent>
+      <Grid container alignItems="center" className={classes.root1}>
+      
+        <div className={classes.content}>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          {text.reportName}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+        </div>
+
+         <Divider orientation="vertical" flexItem />
+         <div style={{paddingLeft:"10%"}}>
+           <img src={star} />
+           <img src={star} />
+           <img src={star} />
+        </div>
+        </Grid>
+        
+      </CardContent>
+     </Card>
+))
     }
 
-    const handleRadioButton = (event) => {
-        setRadiButton(event.target.value);
-    }
+    <div style={{float:"right",marginRight:"5%",marginTop:"2%"}}>
+        <Pagination count={reportList.length%5} onChange = {handleChange} />
+    </div>
 
-    return(
-        <div style={{display:"grid", gridTemplateColumns: "20% 80%", "marginLeft": props.drawer ? "20%":"0%"}}>
-         <div>
-            <List>
-             {filterList.map((text, index) => (
-                <div>
-                    <ListItem button key={text} onClick={handleClick(index)}>
-                        <ListItemText primary={text.menu} />
-                        {openMenu[index]==true ? <ExpandLess/> : <ExpandMore/>}
-                    </ListItem>
-                    <Collapse in = {openMenu[index]} timeout="auto" unmountOnExit>
-                        <List component="div">
-                            {text.type=="list" && List.subMenu.map(x => {
-                                return(
-                                    <ListItem button key={x.name} className={classes.nested}>
-                                        <ListItemText primary={x.name} />
-                                    </ListItem>
-                                )
-                            })}
-
-                            {text.type=="checkbox" && text.subMenu.map((x,i) => {
-                                return(
-                                    <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            checked={checked[i]}
-                                            onChange={handleCheckbox(i)}
-                                            color = "primary"
-                                        />
-                                        }
-                                        label={x.name}
-                                        style={{marginLeft:"15px"}}
-                                    />
-                                )
-                            })}
-
-                            {text.type == "radiobutton" && text.subMenu.map((x,i) => {
-                                return(
-                                    <FormControl component="fieldset"
-                                    style={{marginLeft:"25px"}}>
-                                    <RadioGroup value={radioValue} onClick={handleRadioButton} onChange={handleRadioButton}>
-                                        <FormControlLabel value={x.name} control={<Radio/>} label={x.name} />
-                                    </RadioGroup>
-                                    </FormControl>
-                                )
-                            })}
-                        </List>
-                        </Collapse>
-                </div>
-             ))}   
-            </List>
-        </div>   
+    </div>
 
 
-        <div>
-            {reportList.slice(firstIndex,lastIndex).map((text,index) => (
-                <Card className={classes.root}>
-                    <CardContent>
-                        <Grid container alignItems="center" clsName={classes.root1}>
-                            <div className={classes.content}>
-                                <Typography className={classes.title} color="textSecondry" gutterBottom>
-                                    {text.reportName}
-                                </Typography>
-
-                            </div>
-
-                            <Divider orientation="vertical" flexItem />
-                            <div style={{paddingLeft:"10%"}}>
-                                <img src={star}/>
-                                <img src={star}/>
-                                <img src={star}/>
-                            </div>
-                        </Grid>
-                    </CardContent>
-                </Card>
-            ))}
-
-            <div style={{float:"right", marginRight:"5%", marginTop:"2%"}}>
-                <Pagination count={reportList.length%5} onChange={handleChange} />
-            </div>
-        </div>
-        </div>
+   
+</div>
     )
 }
